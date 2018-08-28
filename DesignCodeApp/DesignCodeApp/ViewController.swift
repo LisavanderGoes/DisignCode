9dc06aca6adc578b0f7c4221cf87a16d0c2e5c81
Lisa <lisavandergoes@gmail.com>
2018-08-28 14:37:21 +0200

    Finished Parllax Animation

--- DesignCodeApp/DesignCodeApp/ViewController.swift
+++ DesignCodeApp/DesignCodeApp/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deviceImageView: UIImageView!
+    @IBOutlet weak var playVisualEffectView: UIVisualEffectView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var bookView: UIView!
+    @IBOutlet weak var heroView: UIView!
+    
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
+        let url = URL(string: urlString)
+        let player = AVPlayer(url: url!)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true){
+            player.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+        
+        titleLabel.alpha = 0
+        deviceImageView.alpha = 0
+        playVisualEffectView.alpha = 0
+        
+        UIView.animate(withDuration: 1){
+            self.titleLabel.alpha = 1
+            self.deviceImageView.alpha = 1
+            self.playVisualEffectView.alpha = 1
+            
+        }
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UIScrollViewDelegate{
+    
+    func scrollViewDidScroll( _ scrollView: UIScrollView){
+        print(scrollView)
+        let offsetY = scrollView.contentOffset.y
+        if offsetY < 0{
+            heroView.transform = CGAffineTransform(translationX: 0, y: offsetY)
+            playVisualEffectView.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            titleLabel.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/4)
+            backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/5)
+        }
     }
-
-
 }
 
